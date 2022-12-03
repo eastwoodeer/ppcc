2a157d20ecedbd2958fac2740d7961f0baee2005
Chenbin <chen.bin11@zte.com.cn>
2022-12-03 12:18:40 +0800

    support "+" and "-" operator

--- main.c
+++ main.c
@@ -8,9 +8,29 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	char *p = argv[1];
+
 	printf(".global main\n");
 	printf("main:\n");
-	printf("    li 3, %d\n", atoi(argv[1]));
+	printf("    li 3, %ld\n", strtol(p, &p, 10));
+
+	while (*p) {
+		if (*p == '+') {
+			p++;
+			printf("    addi 3, 3, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		if (*p == '-') {
+			p++;
+			printf("    subi 3, 3, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "unexpected character: '%c'\n", *p);
+		return 1;
+	}
+
 	printf("    blr\n");
 
     return 0;
--- test.sh
+++ test.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 assert() {
-	expected=$2
 	input=$1
+	expected=$2
 
 	./ppcc "$input" > tmp.s
 	powerpc-linux-gnu-gcc -static -o tmp tmp.s
 	qemu-ppc ./tmp
 	actual="$?"
 
-	if [ "$input" = "$actual" ]; then
+	if [ "$expected" = "$actual" ]; then
 		echo "$input => $actual, pass"
 	else
 		echo "$input => $actual, $expected expected got $actual, failed"
 		exit 1
 	fi
+
+	rm -f ./tmp
 }
 
 assert 0 0
 assert 99 99
+assert '3+12-8' 7
+assert '3+99-8' 94
